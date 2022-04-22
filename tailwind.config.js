c7330d043a8260aacbb1ea7b90137965282431f5
Shehbaz33 <shehbazsayed33@gmail.com>
2022-04-22 14:19:02 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#fff5ec',
+        'secondary': '#f16775',
+        'accents': '#f16c66',
+        'font': '#576a53'
+      },
+    },
+    fontFamily: {
+      poppins: ['Poppins']
+    },
+  },
+  plugins: [],
+}
